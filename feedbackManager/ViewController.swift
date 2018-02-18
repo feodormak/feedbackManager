0283e09db14727aa0b8b31b4f66a1a73ea8375d3
feodor <feodor.mak@gmail.com>
2018-02-18 22:06:52 +0800

    d

--- feedbackManager/ViewController.swift
+++ feedbackManager/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FeedbackManagerViewController: UITableViewController {
+class FeedbackManagerViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
